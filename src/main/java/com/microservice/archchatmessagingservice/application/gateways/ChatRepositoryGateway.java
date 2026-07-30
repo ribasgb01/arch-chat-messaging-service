10686951820f634606631bcf6d65762d944e90fd
ribasgb01 <ribasgb01@gmail.com>
2026-07-30 00:10:34 -0300

    feat: implement chat and message repository gateway interfaces

--- src/main/java/com/microservice/archchatmessagingservice/application/gateways/ChatRepositoryGateway.java
+++ src/main/java/com/microservice/archchatmessagingservice/application/gateways/ChatRepositoryGateway.java
@@ -1,4 +1,15 @@
 package com.microservice.archchatmessagingservice.application.gateways;
 
+import com.microservice.archchatmessagingservice.domain.Chat;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ChatRepositoryGateway {
+    Chat save (Chat chat);
+    Optional<Chat> findById(UUID id);
+    Optional<Chat> findDirectChatBetween(UUID user1, UUID user2);
+    List<Chat> findChatsByUserId(UUID userId);
+    void deleteChat(String chatId);
 }
--- src/main/java/com/microservice/archchatmessagingservice/application/gateways/MessageRepositoryGateway.java
+++ src/main/java/com/microservice/archchatmessagingservice/application/gateways/MessageRepositoryGateway.java
@@ -1,4 +1,13 @@
 package com.microservice.archchatmessagingservice.application.gateways;
 
+import com.microservice.archchatmessagingservice.domain.Message;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface MessageRepositoryGateway {
+
+    Message save(Message message);
+    List<Message> findMessagesByChatId(String chatId);
+    Optional<Message> findById(String messageId);
 }
